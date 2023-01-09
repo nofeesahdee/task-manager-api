f0ed453acbc54123f6f0ba5a93f5e700657d8f0a
Dolapo <dolapo.olatunji@flutterwavego.com>
2023-01-09 13:18:16 +0100

    refactor

--- src/index.js
+++ src/index.js
@@ -2,27 +2,29 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
+const Task = require('./models/task')
+const User = require('./models/user')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-// const multer = require('multer')
-// const upload = multer({
-//     dest: 'images',
-//     limits: {
-//         fileSize: 1000000
-//     },
-//     fileFilter(req, file, cb) {
-//         if (!file.originalname.match(/\.(doc|docx)$/)) {
-//             return cb(new Error('Please upload a Word document'))
-//         }
+const multer = require('multer')
+const upload = multer({
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word document'))
+        }
 
-//         cb(undefined, true)
-//     }
-// })
-// app.post('/upload', upload.single('upload'), (req, res) => {
-//     res.send()
-// })
+        cb(undefined, true)
+    }
+})
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
 
 
 app.use(express.json())
@@ -33,9 +35,8 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-// const Task = require('./models/task')
-// const User = require('./models/user')
 
-// const main = async () => {}
 
-// main()
+const main = async () => {}
+
+main()
